399789ade76288c52f24fe1072fc14facf4b1261
KaizerCricket <44206532+KaizerCricket@users.noreply.github.com>
2020-10-07 13:22:56 -0400

    Added Unity to Github
    Has working main menu scene, incomplete Maze, player movement, collisions, and camera movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 5.0F;
+
+    void Update() {
+        CharacterController controller = GetComponent<CharacterController>();
+
+        controller.SimpleMove(transform.TransformDirection(Vector3.forward) * speed * Input.GetAxis("Vertical")
+                            + transform.TransformDirection(Vector3.right) * speed * Input.GetAxis("Horizontal"));
+    }
+}
